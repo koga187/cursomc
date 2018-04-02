537c50cdb035d4bbb97d06c630881daf8043d628
Bruno Koga <brunokoga187@gmail.com>
2018-04-02 19:20:48 -0300

    Produtos e associação de produtos com categorias

---
+++ src/main/java/com/brunokoga/cursomc/services/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.brunokoga.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunokoga.cursomc.domain.Categoria;
+import com.brunokoga.cursomc.domain.Produto;
+import com.brunokoga.cursomc.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	public Produto buscar(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+
+		return obj.orElse(null);
+	}
+	
+}
