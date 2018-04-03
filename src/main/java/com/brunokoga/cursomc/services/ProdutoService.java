28e84818564546b15eab337c23a826ac3c941198
Bruno Koga <brunokoga187@gmail.com>
2018-04-02 21:04:35 -0300

    Ajuste do get categoria e adicionado exception customizada

--- src/main/java/com/brunokoga/cursomc/services/ProdutoService.java
+++ src/main/java/com/brunokoga/cursomc/services/ProdutoService.java
@@ -2,10 +2,10 @@ package com.brunokoga.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.brunokoga.cursomc.domain.Categoria;
 import com.brunokoga.cursomc.domain.Produto;
 import com.brunokoga.cursomc.repositories.ProdutoRepository;
 
@@ -18,7 +18,8 @@ public class ProdutoService {
 	public Produto buscar(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
 
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(Produto.class,
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
 	}
 	
 }
